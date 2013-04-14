f814e5bfc0e812430018939412ccb603a2094169
astesnyagin@gmail.com <astesnyagin@gmail.com@8e15d958-0f09-b84f-4417-3485adedf3e3>
2013-04-14 21:57:33 +0000

    выполнение команды ОС + логгирование на посмотреть

--- trunk/jposapplet/src/jpos/applet/JposApplet.java
+++ trunk/jposapplet/src/jpos/applet/JposApplet.java
@@ -1,16 +1,25 @@
 package jpos.applet;
 
 import java.applet.Applet;
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.io.UnsupportedEncodingException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import jpos.BaseJposControl;
 import netscape.javascript.JSException;
 import netscape.javascript.JSObject;
@@ -21,31 +30,116 @@ public class JposApplet extends Applet {
     private static final PrintStream out = System.out;
     private static boolean loaded = false;
     private static Applet applet = null;
+    private final String nl = System.getProperty("line.separator");
+    private static Pattern p[] = null;
+    private static String r[] = null;
 
-    private static PrintStream createLoggingProxy(final PrintStream ps) {
+    static {
+        try {
+            p = new Pattern[4];
+            r = new String[4];
+            p[0] = Pattern.compile("\\D\\d{12,16}\\D");
+            p[1] = Pattern.compile("key=\"\\S*\"");
+            p[2] = Pattern.compile("PINBlock=\"\\S*\"");
+            p[3] = Pattern.compile("Track2=\"\\S*\"");
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private static String panmask(final String message) {
+        if (message == null) {
+            return null;
+        }
+        String result = message;
+        for (int i = 0; i < p.length; ++i) {
+            Matcher m = p[i].matcher(result);
+            StringBuffer sb = new StringBuffer();
+            while (m.find()) {
+                if (i == 0) {
+                    m.appendReplacement(sb, result.substring(m.start(), m.start() + 7) + "******" + result.substring(m.end() - 5, m.end()));
+                } else {
+                    m.appendReplacement(sb, r[i]);
+                }
+
+            }
+            m.appendTail(sb);
+            result = sb.toString();
+        }
+        return result;
+
+    }
+
+    public static void consoleOutput(final String logMethod, final String str) {
+        consoleOutput(logMethod, str, 2);
+    }
+    private static SimpleDateFormat sdf;
+
+    static {
+        try {
+            sdf = new SimpleDateFormat("dd.mm.yy HH:MM:SS");
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public static void consoleOutput(final String logMethod, final String str, int targetMethodLevel) {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        String message = "";
+        if (stackTraceElements.length >= targetMethodLevel + 1) {
+            StackTraceElement element = stackTraceElements[targetMethodLevel];
+            String className = element.getClassName();
+            String methodName = element.getMethodName();
+            message = className + "." + methodName + "(" + element.getLineNumber()  + ")";
+        }
+        try {
+            JSObject window = JSObject.getWindow(applet);
+            JSObject console = (JSObject) window.getMember("console");
+            if (console != null) {
+
+                console.call(logMethod, new Object[]{sdf.format(new GregorianCalendar().getTime()) + " " + (!message.startsWith("jpos.util.tracing.Tracer") ? message + " " : "") + str});
+            }
+        } catch (JSException e) {
+        }
+    }
+
+    private static PrintStream createLoggingProxy(final PrintStream ps, final String logMethod) {
 
         return new PrintStream(ps) {
             @Override
             public void print(final String str) {
                 if (str != null) {
                     ps.print(str);
-                    try {
-                        JSObject window = JSObject.getWindow(applet);
-                        JSObject console = (JSObject) window.getMember("console");
-                        if (console != null) {
-                            console.call("log", new Object[]{str});
-                        }
-                    } catch (JSException e) {
-                    }
+                    consoleOutput(logMethod, str,4);
                 }
             }
         };
     }
 
+    public static void log(String message) {
+        consoleOutput("log", message, 3);
+    }
+
+    public static void logError(String message) {
+        consoleOutput("error", message, 3);
+    }
+
+    public static void logError(String message, Throwable t) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream(1024);
+        PrintWriter pw = new PrintWriter(baos);
+        t.printStackTrace(pw);
+        pw.flush();
+        try {
+            consoleOutput("error", message + "\n" + t.getMessage() + "\n" + baos.toString("UTF8"), 3);
+        } catch (UnsupportedEncodingException e) {
+            logError(e.toString());
+        }
+    }
+
     private static void checkProperties() {
         synchronized (Runtime.getRuntime()) {
             if (!loaded) {
-                System.out.println("Current directory is '" + getCurrentDirectory() + "'");
+                log("Current directory is '" + getCurrentDirectory() + "'");
                 try {
                     try (InputStream is = new FileInputStream("jpos.properties")) {
                         Properties properties = new Properties();
@@ -53,7 +147,7 @@ public class JposApplet extends Applet {
                         for (String key : properties.stringPropertyNames()) {
                             System.setProperty(key, properties.getProperty(key));
                         }
-                        System.out.println("Properties are loaded from 'jpos.properties'");
+                        log("Properties are loaded from 'jpos.properties'");
                     }
                 } catch (Throwable e) {
                     System.err.println("Error loading file 'jpos.properties'");
@@ -63,13 +157,27 @@ public class JposApplet extends Applet {
         }
     }
 
+    /**
+     * @return the defaultExecCharsetName
+     */
+    public static String getDefaultCharsetName() {
+        return defaultCharsetName;
+    }
+
+    /**
+     * @param aDefaultExecCharsetName the defaultExecCharsetName to set
+     */
+    public static void setDefaultCharsetName(String aDefaultExecCharsetName) {
+        defaultCharsetName = aDefaultExecCharsetName;
+    }
+
     @Override
     public void start() {
         synchronized (Runtime.getRuntime()) {
             if (applet == null) {
                 applet = this;
-                System.setErr(createLoggingProxy(JposApplet.err));
-                System.setOut(createLoggingProxy(JposApplet.out));
+                System.setErr(createLoggingProxy(JposApplet.err, "log"));
+                System.setOut(createLoggingProxy(JposApplet.out, "log"));
             }
         }
         super.start();
@@ -92,7 +200,7 @@ public class JposApplet extends Applet {
         String deviceClassName = "jpos.applet." + deviceName;
         Class deviceClass = Class.forName(deviceClassName);
         BaseJposControl dev = (BaseJposControl) deviceClass.newInstance();
-        System.out.print("Device '" + deviceName + "' created");
+        log("Device '" + deviceName + "' created");
         return dev;
     }
 
@@ -145,10 +253,9 @@ public class JposApplet extends Applet {
         }
     }
 
-    public static void exec(String command) throws IOException {
-        Runtime.getRuntime().exec(command);
-    }
-
+//    public static void exec(String command) throws IOException {
+//        Runtime.getRuntime().exec(command);
+//    }
     public static void load(String fileName) throws IOException {
         Runtime.getRuntime().load(fileName);
     }
@@ -159,5 +266,82 @@ public class JposApplet extends Applet {
 
     public static String getProperty(String key) {
         return System.getProperty(key);
+    }
+    /**
+     * Вычитыватель потоков вывода - если не вычитывать отдельными потоками то
+     * может подвисать....
+     */
+    private static String defaultCharsetName = "cp866";
+
+    private static final class StreamGrabber extends Thread {
+
+        InputStream is;
+        String type;
+        StringBuilder out = new StringBuilder("");
+
+        StreamGrabber(InputStream is, String type) {
+            super("OSExecutor$" + type);
+            this.is = is;
+            this.type = type;
+        }
+
+        @Override
+        public void run() {
+            try {
+                final InputStreamReader inputStreamReader = new InputStreamReader(is, defaultCharsetName);
+                final BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    out.append(line).append("\n");
+                }
+            } catch (IOException ioe) {
+                ioe.printStackTrace(System.err);
+            }
+        }
+
+        @Override
+        public String toString() {
+            return out.toString();
+        }
+    }
+
+    public static void exec(final String commandLine, final JSObject event) {
+        System.out.println("enter OS exec");
+        new Thread("OSExecutor") {
+            public void run() {
+                try {
+                    String cmdLine1 = commandLine; //"cmd  /C" + commandLine; //"c:\\temp\\mkftp.cmd mkftp.cmd";
+
+                    log("Начало выполнение команды OS:" + cmdLine1);
+
+                    Process p = Runtime.getRuntime().exec(cmdLine1);
+                    StreamGrabber err = new StreamGrabber(p.getErrorStream(), "ERROR");
+                    err.start();
+                    StreamGrabber out = new StreamGrabber(p.getInputStream(), "OUTPUT");
+                    out.start();
+                    int status = -99;
+                    while (true) {
+                        try {
+                            status = p.waitFor();
+                            break;
+                        } catch (InterruptedException ie) {
+                            log("child process output reader interrupted");
+                            p.destroy();
+                            return;
+                        }
+                    }
+                    log("Выполнена команда OS:" + commandLine + "  ,status=" + status);
+                    if (event != null) {
+                        event.call("call", new Object[]{applet, status, out.toString(), err.toString(), commandLine});
+                    }
+
+                } catch (Throwable ex) {
+                    logError("Ошибка при выполнении команды OS:" + ex.getMessage());
+                    if (event != null) {
+                        event.call("call", new Object[]{applet, null, null, ex.getMessage(), commandLine});
+                    }
+                }
+            }
+        }.start();
     }
 }
